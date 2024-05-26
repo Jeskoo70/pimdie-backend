4bb1c59d6ba6a5b51d40797e26d1df3b211341a6
Jeskoo <gera.steam@mail.ru>
2024-05-26 13:43:22 +1000

    добавили функционал создания данных

--- routes/categories.js
+++ routes/categories.js
@@ -1,8 +1,11 @@
-const sendAllCategories = require("../controllers/categories");
-const findAllCategories = require("../middlewares/categories");
+const {sendAllCategories, sendCategoryById, sendCategoryCreated} = require("../controllers/categories");
+const {findAllCategories, findCategoryById, createCategory} = require("../middlewares/categories");
 
 const categoriesRouter = require("express").Router();
 
 categoriesRouter.get("/categories",findAllCategories, sendAllCategories);
+categoriesRouter.get("/categories/:id", findCategoryById, sendCategoryById);
+categoriesRouter.post("/categories", findAllCategories, createCategory, sendCategoryCreated)
+
 
 module.exports = categoriesRouter;
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
-const sendAllUsers = require("../controllers/users");
-const findAllUsers = require("../middlewares/users");
+const { sendAllUsers, sendUserById, sendUserCreated } = require("../controllers/users");
+const { findAllUsers, findUserById, createUser } = require("../middlewares/users");
 
 const usersRouter = require("express").Router();
 
 usersRouter.get("/users",findAllUsers, sendAllUsers);
+usersRouter.get("/users/:id", findUserById, sendUserById);
+usersRouter.post("/users", findAllUsers, createUser, sendUserCreated);
 
 module.exports = usersRouter;
